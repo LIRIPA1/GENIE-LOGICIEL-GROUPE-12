d5b47d3ebf59fb7fa6bbc8ddee2f2396722c1de5
groupe12 <groupe12@gmail.com>
2025-03-28 15:01:01 +0100

    KYAKIMWA NGELEZA

--- TP2 GENIE LOGICIEL GP12/TP2/Program.cs
+++ TP2 GENIE LOGICIEL GP12/TP2/Program.cs
@@ -19,7 +19,7 @@ namespace TP2
 
             Console.WriteLine("ABSTRACT CLASS");
             Console.WriteLine("===============\n");
-            Teacher t1 = new Teacher("lofo", "pauni", "22LSILJ620066");
+            Teacher t1 = new Teacher("KYAKIMWA", "ngleza", "22LSILJ620023");
             Student s1 = new Student("LIRIPA" , "KPAKI", "22LSILJ670206");
 
             t1.Add(2);
