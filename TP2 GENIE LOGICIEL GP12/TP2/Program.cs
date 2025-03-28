1dfe1c16fdf2131831ba7c66e5fd0ba18bb228ce
groupe12 <groupe12@gmail.com>
2025-03-28 15:04:45 +0100

    KOFUNDA MERVEILLE

--- TP2 GENIE LOGICIEL GP12/TP2/Program.cs
+++ TP2 GENIE LOGICIEL GP12/TP2/Program.cs
@@ -19,7 +19,7 @@ namespace TP2
 
             Console.WriteLine("ABSTRACT CLASS");
             Console.WriteLine("===============\n");
-            Teacher t1 = new Teacher("KYAKIMWA", "ngleza", "22LSILJ620023");
+            Teacher t1 = new Teacher("KOFUNDA", "MERVEILLE", "22LSILJ800242");
             Student s1 = new Student("LIRIPA" , "KPAKI", "22LSILJ670206");
 
             t1.Add(2);
