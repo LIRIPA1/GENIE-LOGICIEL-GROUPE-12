538a3dcb3e07527a501c7cce248e37793f15ae0f
groupe12 <groupe12@gmail.com>
2025-03-28 14:56:22 +0100

    LOFO PAUNI

--- TP2 GENIE LOGICIEL GP12/TP2/Program.cs
+++ TP2 GENIE LOGICIEL GP12/TP2/Program.cs
@@ -19,7 +19,7 @@ namespace TP2
 
             Console.WriteLine("ABSTRACT CLASS");
             Console.WriteLine("===============\n");
-            Teacher t1 = new Teacher("LONGA", "LALONGA", "22LSILJ619999");
+            Teacher t1 = new Teacher("lofo", "pauni", "22LSILJ620066");
             Student s1 = new Student("LIRIPA" , "KPAKI", "22LSILJ670206");
 
             t1.Add(2);
